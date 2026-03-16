dca5818cb80779c6acfd6bf7cf6661b55b40d41d
Nguyen Hoang Vinh - CNTT <vinhnh1@kienlongbank.com>
2026-03-16 13:30:50 +0700

    Implement Bubble Sort algorithm and add main function for testing

--- 01-foundation/cs/algorithms/01-sorting/bubble_sort.go
+++ 01-foundation/cs/algorithms/01-sorting/bubble_sort.go
@@ -1,8 +1,27 @@
 package main
 
+import "fmt"
 // BubbleSort sắp xếp mảng theo thứ tự tăng dần.
 // Tự triển khai thân hàm như bài tập.
 func BubbleSort(nums []int) {
-	// TODO: implement bubble sort
+	n := len(nums)
+	for i := 0; i < n-1; i++ {
+		swapped := false
+		for j := 0; j < n-i-1; j++ {
+			if nums[j] > nums [j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
+		}
+	}
+}
+
+func main() {
+	nums := []int{5, 3, 8, 4, 2}
+	BubbleSort(nums)
+	fmt.Println(nums)
 }
 
